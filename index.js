3418a7ed98d385aaf841948005879c1b2cd6dfa5
TheHypernova <saturnmfgg@gmail.com>
2020-09-20 01:28:09 -0500

    Finished a mini-side project - IP autocompleter

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ app.get('/', function (req, res, queryCallback) {
     });
 })
 
+app.get('/iptest', function (req, res, queryCallback) {
+    const ipimport = require('./iptester.js');
+    const IP = new ipimport();
+    const num = '1.1.1.1';
+    const num2 = '1.1.1.14';
+    const num3 = 'avbagf12';
+    Results = IP.ipAddressAutocomplete(num);
+    Results2 = IP.ipAddressAutocomplete(num2);
+    Results3 = IP.ipAddressAutocomplete(num3);
+    res.send(`
+        Filtered IP of ${num.toString()}     -     ${Results}<br/>
+        Filtered IP of ${num2.toString()}     -     ${Results2}<br/>
+        Filtered IP of ${num3.toString()}     -     ${Results3}<br/>
+    `)
+})
+
 var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
--- iptester.js
+++ iptester.js
@@ -1,12 +1,55 @@
-function expectedNotationRange (input = 0) {
-	if (input === null) return ['0-255'];
-	if (input === 0) return ['0'];
-	let stringInput = input.toString();
-	let possibleNumbers = [];
-	for (let i = 0; i < 3; i ++) {
-		let tempNumber = stringInput;
-        stringInput += (stringInput.length < 3) ? '0' : '';
-        possibleNumbers
-	}
-	
+class IP {
+
+    ipAddressAutocomplete(input = '') {
+        if (input === null) return ''; //null
+        if (input.match(/[^(0-9|.)]/)) return input; //NOT an IP address
+        input = input.toString();
+        let ipArray = input.split('.');
+        const numberOnTheRight = ipArray[ipArray.length - 1];
+        let exceptNumberOnRight = '';
+        let remainder = '';
+        for (let i = 0; i < 3; i ++) {
+            exceptNumberOnRight += (i <= ipArray.length - 2) ? ipArray[i] : '';
+            exceptNumberOnRight += (i <= ipArray.length - 2) ? '.' : '';
+
+            remainder += (i >= ipArray.length - 1) ? '.0-255' : '';
+        }
+        
+        const possibilities = this.expectedNotationRange(numberOnTheRight);
+        let rawOutput = [];
+        for (let i = 0; i < possibilities.length; i ++) {
+            rawOutput.push(exceptNumberOnRight + possibilities[i] + remainder);
+        }
+
+        return rawOutput.join(', ');
+    }
+
+    expectedNotationRange (input = 0) {
+        if (input === null) return ['0-255'];
+        if (input === 0) return ['0'];
+        let stringInput = input.toString();
+        let possibleNumbers = [stringInput];
+        for (let i = 0; i < 3; i ++) {
+            if (stringInput.length < 3) {
+                stringInput += '0';
+                if (parseFloat(stringInput) < 255) {
+                    possibleNumbers.push(stringInput);
+                }
+            }
+        }
+        possibleNumbers.forEach((number, index) => {
+            if (index > 0) {
+                const originalNumber = number;
+                let add = '0';
+                for (let a = 0; a < number.split("0").length - 1; a ++) {
+                    add += '9';
+                }
+                let maxNum = originalNumber + Math.min(parseFloat(number) + parseFloat(add), 255).toString();
+                possibleNumbers[index] = `${number}-${maxNum.slice(number.length)}`;
+            }
+        });
+        return possibleNumbers;
+    }
 }
+
+module.exports = IP;
