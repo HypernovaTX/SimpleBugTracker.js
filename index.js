24542077352064f86a17db60f8446c253fe047d4
TheHypernova <saturnmfgg@gmail.com>
2020-09-27 22:01:19 -0500

    Moved GET Request to POST request and tested that it's working properly.

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ app.use(cors());
 const dbimport = require('./src/dbdriver.js');
 const db = new dbimport();
 const CONFIG = require("./config.json");
+const { isUndefined } = require('util');
 let get_output = '';
 
-app.get('/', function (req, res, queryCallback) {
+/*app.get('/', function (req, res, queryCallback) {
     db.db_connect();
     let query = db.db_buildquery_select([
         't.*',
@@ -22,6 +23,8 @@ app.get('/', function (req, res, queryCallback) {
     query += db.db_buildquery_join('status', 's', 'ON (s.stid = t.status)');
     query += db.db_buildquery_join('priority', 'p', 'ON (p.prid = t.priority)');
     query += db.db_buildquery_order(['tid'],[true]);
+    console.log('Req is: VVVV');
+    console.log(req);
     console.log(`QUERY ---------- ${query}`);
     //make a callback for the database
     db.db_query(query, (rows) => {
@@ -30,21 +33,36 @@ app.get('/', function (req, res, queryCallback) {
         db.db_disconnect();
         res.end();
     });
-})
+})*/
 
-/*axios.post('/list', data, {
-    headers: {
-        'key': CONFIG.api.key,
-        'Accept' : 'application/json',
-        'Content-Type': 'application/json'
+app.use(express.json());
+
+app.post('/', function (request, response, queryCallback) {
+    db.db_connect();
+    if (request.body !== undefined) {
+        console.log(request.body.test);
     }
+    let query = db.db_buildquery_select([
+        't.*',
+        'u.name AS username',
+        's.name AS statusname',
+        's.color AS statuscolor',
+        'p.name AS priorityname',
+        'p.color AS prioritycolor'
+    ], 't', 'tickets');
+    query += db.db_buildquery_join('users', 'u', 'ON (u.uid = t.uid)');
+    query += db.db_buildquery_join('status', 's', 'ON (s.stid = t.status)');
+    query += db.db_buildquery_join('priority', 'p', 'ON (p.prid = t.priority)');
+    query += db.db_buildquery_order(['tid'],[true]);
+
+    //make a callback for the database
+    db.db_query(query, (rows) => {
+        if (!rows.length) { queryCallback(response.send('["No results!"]')); }
+        queryCallback(response.send(`${JSON.stringify(rows)}`));
+        db.db_disconnect();
+        response.end();
+    });
 })
-.then(response => {
-    return  response;
-})
-.catch((error) => {
-    return  error;
-});*/
 
 var server = app.listen(8080, function () {
     var host = server.address().address;
